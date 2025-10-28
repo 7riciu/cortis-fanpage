4cbcced1e547991ccebdcf0fcf8976951c2bda9a
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-10-28 13:14:15 +0200

    I added the image slider to all of the members' profile and made slight adjustments :)

--- members.js
+++ members.js
@@ -3,9 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const slides = document.querySelectorAll(".slide");
     console.log(slides.length);
 
-    window.changeSlide = function(direction) {
+    const leftArrow = document.querySelector(".prev");
+    const rightArrow = document.querySelector(".next");
+
+    function changeSlide(direction) {
         slides[imageIndex].classList.remove("active");
         imageIndex = (imageIndex + direction + slides.length) % slides.length;
         slides[imageIndex].classList.add("active");
     }
+
+    leftArrow.addEventListener("click", () => changeSlide(-1));
+    rightArrow.addEventListener("click", () => changeSlide(1));
 })
