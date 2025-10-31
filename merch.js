2d7f75d0ec3b11571d773c8c1d0b115c9e3f5123
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-10-31 23:51:37 +0200

    Im doing something different

---
+++ merch.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var discographyImage = document.getElementById("discography-image");
+    discographyImage.addEventListener("mouseenter", () => {
+        discographyImage.src = "images/music-hover.png";
+    });
+    discographyImage.addEventListener("mouseleave", () => {
+        discographyImage.src = "images/music.png";
+    });
+
+    var membersImage = document.getElementById("members-image");
+    membersImage.addEventListener("mouseenter", () => {
+        membersImage.src = "images/members-hover.png";
+    });
+    membersImage.addEventListener("mouseleave", () => {
+        membersImage.src = "images/members.png";
+    });
+
+    var merchImage = document.getElementById("merch-image");
+    merchImage.addEventListener("mouseenter", () => {
+        merchImage.src = "images/merch-hover.png";
+    });
+    merchImage.addEventListener("mouseleave", () => {
+        merchImage.src = "images/merch.png";
+    });
+
+    var minigameImage = document.getElementById("minigame-image");
+    minigameImage.addEventListener("mouseenter", () => {
+        minigameImage.src = "images/console-hover.png";
+    });
+    minigameImage.addEventListener("mouseleave", () => {
+        minigameImage.src = "images/console.png";
+    });
+});
