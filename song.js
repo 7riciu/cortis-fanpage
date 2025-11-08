12ede908a3ceafbc48e5333b82c4db7ec8049b55
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-11-08 19:19:55 +0200

    Now you get a message when you press each key + layout changes

--- song.js
+++ song.js
@@ -33,4 +33,36 @@ document.addEventListener("DOMContentLoaded", () => {
         if (audio.paused) playSong();
         else pauseSong();
     })
+
+    // Piano
+    Tone.start();
+    const synth = new Tone.Synth().toDestinaion;
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "A" || e.key === "a") {
+            console.log("Pressed A");
+        }
+        else if (e.key === "S" || e.key === "s") {
+            console.log("Pressed S");
+        }
+        else if (e.key === "D" || e.key === "d") {
+            console.log("Pressed D");
+        }
+        else if (e.key === "F" || e.key === "f") {
+            console.log("Pressed F");
+        }
+        else if (e.key === "G" || e.key === "g") {
+            console.log("Pressed G");
+        }
+        else if (e.key === "H" || e.key === "h") {
+            console.log("Pressed H");
+        }
+        else if (e.key === "J" || e.key === "j") {
+            console.log("Pressed J");
+        }
+    });
+
+    function playNote(note) {
+        synth.triggerAttackRelease(`${note}4`, '8n');
+    }
 });
